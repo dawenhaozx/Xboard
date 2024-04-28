d880b500bf872837a3cddf824813546b10c2a76b
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-04-28 10:45:51 +0800

    Update UniProxyController.php

--- app/Http/Controllers/V1/Server/UniProxyController.php
+++ app/Http/Controllers/V1/Server/UniProxyController.php
@@ -197,24 +197,33 @@ class UniProxyController extends Controller
             return ServerService::getAvailableUsers($request->input('node_info')->group_id);
         });
         // 构建需要更新的缓存数据
-        foreach ($users as $user) {
-            $userId = $user->id;
-            $ipsData = $requestData[$userId] ?? [];
-            $cachedIpsData = Cache::get('ALIVE_IP_USER_' . $userId) ?? [];
-            $cachedIpsData[$nodeTypeId] = ['aliveips' => $ipsData];
+        $updateAt = time();
+        foreach ($users as $user => $id) {
+            if (!isset($requestData[$id])) {
+                $requestData[$id] = [];
+            }
+
+        }
+        foreach ($requestData as $uid => $ips) {
+            $ips_array = Cache::get('ALIVE_IP_USER_' . $uid) ?? [];
+            $ips_array[$nodeTypeId] = ['aliveips' => $ips, 'lastupdateAt' => $updateAt];
 
             // 对同一用户的IP进行去重
             $allAliveIPs = [];
-            foreach($cachedIpsData as $key=>$newips) {
-                if ($newips['aliveips']!=[]){
-                    $allAliveIPs = array_merge($allAliveIPs, $newips['aliveips']);
+            foreach ($ips_array as $key => $value) {
+                if (!is_int($value) && isset($value['aliveips'])) {
+                    if ($updateAt - $value['lastupdateAt'] > 135) {
+                        unset($ips_array[$key]);
+                    } else {
+                        $allAliveIPs = array_merge($allAliveIPs, $value['aliveips']);
+                    }
                 }
             }
 
-            $cachedIpsData['alive_ips'] = array_unique($allAliveIPs);
-            $cachedIpsData['alive_ip'] = count($cachedIpsData['alive_ips']);
+            $ips_array['alive_ips'] = array_unique($allAliveIPs);
+            $ips_array['alive_ip'] = count($ips_array['alive_ips']);
 
-            Cache::put('ALIVE_IP_USER_' . $userId, $cachedIpsData, 3600);
+            Cache::put('ALIVE_IP_USER_' . $uid, $ips_array, 3600);
         }
 
         return $this->success(true);
